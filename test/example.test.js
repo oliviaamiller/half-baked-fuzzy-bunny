8da5453d560b03a2e1f9e905e5234fd378f752de
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-10 16:15:52 -0800

    renderBunny tdd working

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,25 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderBunny } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderBunny is a function that will return a p tag with the class of bunny', async(expect) => {
+
+    const bunny = {
+        name: 'cutie',
+        id: 1
+    };
+
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<p class="bunny">cutie</p>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = await renderBunny(bunny);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected, 'returns .bunny p element');
 });
